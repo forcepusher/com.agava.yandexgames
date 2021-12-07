962d75edf67ed2beb0d2e055705bbf0e6ef17f62
forcepusher <bionitsoup@gmail.com>
2021-12-07 21:55:51 +0700

    Change License to MIT

---
+++ Runtime/Leaderboard/LeaderboardDescriptionResponse.cs
@@ -0,0 +1,57 @@
+using System;
+using UnityEngine.Scripting;
+
+namespace YandexGames
+{
+    [Serializable]
+    public class LeaderboardDescriptionResponse
+    {
+        [field: Preserve]
+        public string appID;
+        //[field: Preserve]
+        //public bool default; // Nice trap, Yandex.
+        [field: Preserve]
+        public Description description;
+        [field: Preserve]
+        public string name;
+        [field: Preserve]
+        public Title title;
+
+
+        [Serializable]
+        public class Description
+        {
+            [field: Preserve]
+            public bool invert_sort_order;
+            [field: Preserve]
+            public ScoreFormat score_format;
+            [field: Preserve]
+            public string type;
+
+
+            [Serializable]
+            public class ScoreFormat
+            {
+                [field: Preserve]
+                public Options options;
+
+
+                [Serializable]
+                public class Options
+                {
+                    [field: Preserve]
+                    public int decimal_offset;
+                }
+            }
+        }
+
+        [Serializable]
+        public class Title
+        {
+            [field: Preserve]
+            public string en;
+            [field: Preserve]
+            public string ru;
+        }
+    }
+}
